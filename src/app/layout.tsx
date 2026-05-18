465bdb7188dea7061ad44de99c2f38818ca6d45d
WuLandon <landonwu@berkeley.edu>
2026-05-18 14:26:27 -0700

    add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/next";
 import type { Metadata } from "next";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Anton, Archivo_Black, Roboto_Mono } from "next/font/google";
@@ -65,6 +66,7 @@ export default function RootLayout({
     >
       <body className="min-h-screen flex flex-col">
         {children}
+        <Analytics />
         <SpeedInsights />
       </body>
     </html>
